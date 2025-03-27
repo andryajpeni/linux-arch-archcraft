e2a8d8b595a15665e39566a247c62781217d2652
Andry Peni <andryajpeni@engineer.com>
2025-03-28 00:22:48 +0800

    Add files via upload

---
+++ bspwm/themes/black/theme.bash
@@ -0,0 +1,84 @@
+# mrnngstr666
+# black
+
+# Colors
+background='#000000'
+foreground='#FFFFFF'
+color0='#000000'
+color1='#ff5555'
+color2='#55ff55'
+color3='#ffff55'
+color4='#5555ff'
+color5='#ff55ff'
+color6='#55ffff'
+color7='#ffffff'
+color8='#555555'
+color9='#a80000'
+color10='#00a800'
+color11='#a85700'
+color12='#0000a8'
+color13='#a800a8'
+color14='#00a8a8'
+color15='#a8a8a8'
+
+accent='#181920'
+color_red='#ff0000'
+color_green='#00ff00'
+color_yellow='#ffff00'
+
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrainsMono Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='JetBrainsMono Nerd Font 10'
+rofi_icon='Hack'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='hack.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='jetBrainsMono Nerd Font 9'
+gtk_theme='Cyberpunk'
+icon_theme='Archcraft-Dark'
+cursor_theme='Qogirr'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x36'
+dunst_origin='top-right'
+dunst_font='jetBrainsMono Nerd Font 10'
+dunst_border='1'
+dunst_separator='1'
+
+# Picom
+picom_backend='glx'
+picom_corner='8'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method=''
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='1'
+bspwm_gap='30'
+bspwm_sratio='0.50'
